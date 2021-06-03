0a08ef259c0a4c67090b117c079180ea56e88981
Austry Castillo <castilloaustry@gmail.com>
2021-06-02 22:16:02 -0300

    actualización limpiar imputs

--- js/clase5.js
+++ js/clase5.js
@@ -10,51 +10,53 @@
 // // //mostrar()
 // // boton.onclick = mostrar
 
-// let alumnos = [
-//     {
-//         "nombre": "Cleopatra",
-//         "edad": 80
-//     },
-//     {
-//         "nombre": "Juan",
-//         "edad": 5
-//     }
+let alumnos = [
+    {
+        "nombre": "Cleopatra",
+        "edad": 80
+    },
+    {
+        "nombre": "Juan",
+        "edad": 5
+    }
 
-// ]
-// function dibujarTabla() {
-//     let tabla = document.querySelector("table")
-//     let tr = document.createElement("tr")
-//     let td = document.createElement("td")
-//     td.innerHTML = "Nombre"
-//     tr.appendChild(td)
-//     td = document.createElement("td")
-//     td.innerHTML = "Edad"
-//     tr.appendChild(td)
-//     tabla.appendChild(tr)
-//     for (var i = 0; i < alumnos.length; i++) {
-//         tr = document.createElement("tr")//creamos el tag html
-//         td = document.createElement("td")
-//         td.innerHTML = alumnos[i].nombre//agregamos o modificamos info
-//         tr.appendChild(td)//confirmar o agregar dentro de un tag
-//         td = document.createElement("td")
-//         td.innerHTML = alumnos[i].edad
-//         tr.appendChild(td)
-//         tabla.appendChild(tr)
-//     }
-// }
-// dibujarTabla()
-// let agregar = document.querySelector("#agregar")
-// agregar.onclick = agregarDato
+]
+function dibujarTabla() {
+    let tabla = document.querySelector("table")
+    let tr = document.createElement("tr")
+    let td = document.createElement("td")
+    td.innerHTML = "Nombre"
+    tr.appendChild(td)
+    td = document.createElement("td")
+    td.innerHTML = "Edad"
+    tr.appendChild(td)
+    tabla.appendChild(tr)
+    for (var i = 0; i < alumnos.length; i++) {
+        tr = document.createElement("tr")//creamos el tag html
+        td = document.createElement("td")
+        td.innerHTML = alumnos[i].nombre//agregamos o modificamos info
+        tr.appendChild(td)//confirmar o agregar dentro de un tag
+        td = document.createElement("td")
+        td.innerHTML = alumnos[i].edad
+        tr.appendChild(td)
+        tabla.appendChild(tr)
+    }
+    document.querySelector("#nombre").value = ""
+    document.querySelector("#edad").value = ""
+}
+dibujarTabla()
+let agregar = document.querySelector("#agregar")
+agregar.onclick = agregarDato
 
-// function agregarDato() {
-//     let tabla = document.querySelector("table")
-//     tabla.innerHTML = ""
-//     let nombre = document.querySelector("#nombre").value
-//     let edad = document.querySelector("#edad").value
-//     //console.log(nombre)
-//     alumnos.push({ 'nombre': nombre, 'edad': edad })
-//     dibujarTabla()
-// }
+function agregarDato() {
+    let tabla = document.querySelector("table")
+    tabla.innerHTML = ""
+    let nombre = document.querySelector("#nombre").value
+    let edad = document.querySelector("#edad").value
+    //console.log(nombre)
+    alumnos.push({ 'nombre': nombre, 'edad': edad })
+    dibujarTabla()
+}
 
 // let btnEncender = document.querySelector("#encender")
 // let btnApagar = document.querySelector("#apagar")
@@ -77,12 +79,12 @@
 //let titulo = $("#titulo").text()
 //let titulo = $(".titu").text()
 //alert(titulo)
-$("#titulo").text('Hola Jquery')
-$("#d").click(desaparecer)
-function desaparecer(){
-    //$('#gato').hide()
-    $('#gato').fadeOut("slow")
-}
-$("#btn").click(function(){
-    $("#content").slideToggle("slow")
-})
+// $("#titulo").text('Hola Jquery')
+// $("#d").click(desaparecer)
+// function desaparecer(){
+//     //$('#gato').hide()
+//     $('#gato').fadeOut("slow")
+// }
+// $("#btn").click(function(){
+//     $("#content").slideToggle("slow")
+// })
